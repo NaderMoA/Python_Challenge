25ed5970e920748466a29fb27842291c303ec543
NaderMoa <nadermoazezi@gmail.com>
2023-11-11 21:02:22 -0500

    final

--- PayBank/main.py
+++ PayBank/main.py
@@ -1,4 +1,5 @@
 import csv
+import os
 filepath = r"PayBank\Resources\budget_data.csv"
 total_months = 1
 Total_profit_loss = []
@@ -7,18 +8,22 @@ low_change = 0
 high_change = 0
 with open(filepath,"r") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
+    #skipping the header
     Header_row = next(csvreader)
+    #skipping the first row to use its value for calculating the net changes of first month
     first_row = next(csvreader)
     prev_net = int(first_row[1])
     Total_net = int(first_row[1])
     for row in csvreader:
-        
+        #calculationg the total months and the total net
         total_months += 1
         Total_net = int(row[1]) + Total_net
+
         #calculating the changes
         changes = int(row[1]) - prev_net
         Net_Changes.append(changes)
         prev_net = int(row[1])
+
         #calculating greatest decrease
         if low_change > changes:
           low_change = changes
@@ -27,11 +32,21 @@ with open(filepath,"r") as csvfile:
         if high_change < changes:
            high_change = changes
            high_month = row[0]  
-        
+ #calculating the Average Change       
 avgchanges = round(sum(Net_Changes)/len(Net_Changes), 2)
-    
+ #printing the results in terminal   
 print(f"Total Months {total_months}")
 print(f"Total ${Total_net}")
 print(f"Average Change ${avgchanges}")
 print(f"Greatest Increase in Profits: {high_month} (${high_change})")
 print(f"Greatest Decrease in Profits: {low_month} (${low_change})")
+#writing into the text file:
+textpath = r"PayBank\analysis\bank.txt"
+with open (textpath,'w') as f:
+   f.write("Financial Analysis\n")
+   f.write("......................\n")
+   f.write(f"Total Months: {total_months}\n")
+   f.write(f"Total: {Total_net}\n")
+   f.write(f"Average Change: {avgchanges}\n")
+   f.write(f"Greatest Increase in Profits: {high_month} $({high_change})\n")
+   f.write(f"Greatest Decrease in Profits: {low_month} $({low_change})\n")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,5 +1,4 @@
 import csv
-import math
 filepath = r"PyPoll\Resources\election_data.csv"
 total_vote = 1
 candidates = []
@@ -10,17 +9,19 @@ Election =[]
 
 with open (filepath, "r") as csvfile:
     csvreader = csv.reader(csvfile,delimiter= ",")
+    #skipping the header row
     header = next(csvreader)
+    #skipping the first row to use its valu and compare it the row below it
     first_row = next(csvreader)
     candid = first_row[2]
-    
+    #finding the list of all candidates
     candidates.append(candid)
     for vote in csvreader:
         total_vote +=1
         if (candid != vote[2]) and (vote[2] not in candidates): 
             candidates.append(vote[2])
 
-    
+     #finding the number of votes for each candidate
         if candidates[0] == vote[2]:
             candid_1 +=1
         elif candidates[1] == vote[2]:
@@ -28,7 +29,7 @@ with open (filepath, "r") as csvfile:
         else:
             candid_3 +=1
 
-
+#calculating the % for each candidate and adding them to a list to later find the max and the winner from the list
 percent_candid_1 = round(((candid_1)/total_vote)*100, 3)
 percent_candid_2 = round(((candid_2)/total_vote)*100, 3)
 percent_candid_3 = round(((candid_3)/total_vote)*100, 3)
@@ -37,20 +38,27 @@ Election.append(float(percent_candid_2))
 Election.append(float(percent_candid_3))
 #identifying the percentage of the winner votes
 win = max(Election)
-
-
-#identifying the candidate who won
+#identifying the candidate who won by coparing the max % to the vote % of each candidate
 num = 0
 for nom in Election:
  if win == nom:
   Winer = candidates[num]
  else:
     num +=1
-
-
-
+#printging the results in Terminal
 print(f"Total Votes: {total_vote}")
 print(f"{candidates[0]}: {percent_candid_1}% ({candid_1})")
 print(f"{candidates[1]}: {percent_candid_2}% ({candid_2})")
 print(f"{candidates[2]}: {percent_candid_3}% ({candid_3})")
 print(f"Winner: {Winer}")
+#writing the results in text file:
+textpath = r"PyPoll\analysis\poll.txt"
+with open(textpath, "w") as f:
+   f.write("Election Results\n")
+   f.write("......................\n")
+   f.write(f"Total Votes: {total_vote}\n")
+   f.write(f"{candidates[0]}: {percent_candid_1}% ({candid_1})\n")
+   f.write(f"{candidates[1]}: {percent_candid_2}% ({candid_2})\n")
+   f.write(f"{candidates[2]}: {percent_candid_3}% ({candid_3})\n")
+   f.write("......................\n")
+   f.write(f"Winner: {Winer}")
